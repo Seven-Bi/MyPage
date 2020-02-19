67f3a9b410208f8796e8c488c0ea72273f4ec991
BB <seven.albany.bi@gmail.com>
2020-02-19 12:41:09 +0800

    gunicorn start api service

--- api/my_api/my_api/settings.py
+++ api/my_api/my_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-!y=t)1o9_&uy9t%rd@!-9pjzu!z#ka+s)^h4sx=0n34fryrt_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'gunicorn',
 ]
 
 MIDDLEWARE = [
