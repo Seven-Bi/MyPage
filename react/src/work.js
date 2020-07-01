d13fb9de81f9cc0d45fde1b3d3ae555405d1d667
BB <seven.albany.bi@gmail.com>
2020-07-01 11:43:21 +0800

    upload image

--- react/src/work.js
+++ react/src/work.js
@@ -3,8 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card'
 import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
-import img1 from './static/img/B.png';
+// import img1 from './static/img/A.jpg';
+import img2 from './static/img/B.png';
+import img3 from './static/img/C.bmp';
 
 
 
@@ -15,7 +18,8 @@ const useStyles = makeStyles((theme) => ({
 		marginBottom: '3vh',
 		backgroundColor: '#F2F2F2',
 	},
-	logo_card: {
+	logo_content: {
+		margin: '0.5vh',
 		backgroundColor: '#292929',
 	},
 	text_content: {
@@ -40,7 +44,23 @@ export default function Work() {
 	const classes = useStyles();
 
 	const work_list = [
-		{'img': {img1}, 'title': 'EEA', 'role': 'software engineer', 'date': 'July 2019 to October 2019', 'description': ['website development', 'api design', 'mobile app UI']},
+		{
+			'img': 'https://raw.githubusercontent.com/Seven-Bi/MyPage/master/react/src/static/img/A.jpg', 
+			'title': 'South Pacific', 'role': 'ICT Developer', 
+			'date': 'February 2020 to June 2020', 
+			'description': ['Website development & maintain', 'Marketing email system development', 'UI design']},
+		{
+			'img': 'https://raw.githubusercontent.com/Seven-Bi/MyPage/master/react/src/static/img/B.png', 
+			'title': 'EEA', 
+			'role': 'Software Engineer', 
+			'date': 'July 2019 to October 2019', 
+			'description': ['Full stack development', 'API design']},
+		{
+			'img': 'https://raw.githubusercontent.com/Seven-Bi/MyPage/master/react/src/static/img/C.bmp', 
+			'title': 'PinYi', 
+			'role': 'Software Engineer', 
+			'date': 'September 2012 to July 2013', 
+			'description': ['system maintain & assist new feature development', 'website maintain']},
 	];
 
 	return (
@@ -51,33 +71,39 @@ export default function Work() {
 	            </Typography>			
 			</div>
 			<div className={classes.text_content}>
-				<div className={classes.work_content}>
-					<Card className={classes.logo_card}>
-						<img src={img1} alt="" width="150" height="150" />
-					</Card>
+				{work_list.map(item => (
+					<div className={classes.work_content}>
+						<Paper elevation={3}>
+							<div className={classes.logo_content}>
+								<img src={item.img} alt="" width="150" height="150" />
+							</div>
+						</Paper>
 
-					<div className={classes.text_content}>
-			            <Typography gutterBottom variant="h4">
-							<strong>EEA</strong>
-			            </Typography>
-			            <Typography gutterBottom variant="subtitle1">
-							Software Engineer
-			            </Typography>
-			            <Typography gutterBottom variant="subtitle2">
-							July 2019 to October 2019
-			            </Typography>
+						<div className={classes.text_content}>
+				            <Typography gutterBottom variant="h5">
+								<strong>{item.title}</strong>
+				            </Typography>
+				            <Typography gutterBottom variant="subtitle1">
+								{item.role}
+				            </Typography>
+				            <Typography gutterBottom variant="subtitle2">
+								{item.date}
+				            </Typography>
 
-			            <Divider variant="middle" />
+				            <Divider variant="middle" />
 
-			            <Typography gutterBottom variant="body1">
-							<ul>
-								<li>website development</li>
-								<li>api design</li>
-								<li>mobile app UI</li>
-							</ul>
-			            </Typography>
-		            </div>
-				</div>	
+				            <Typography gutterBottom variant="body1">
+								<ul>
+								{item.description.map(sub_item => (
+									<li>
+										{sub_item}
+									</li>
+								))}
+								</ul>
+				            </Typography>
+			            </div>
+					</div>	
+				))}
 			</div>
 		</Card>
 	);
