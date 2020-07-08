cdde82fef1ac1f2acec974fa4175f1ec861e052b
BB <seven.albany.bi@gmail.com>
2020-07-08 15:26:01 +0800

    save

---
+++ build/precache-manifest.65c76ad7a67d8d2e119ea698b8b6d74a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf0774b363ca1184add69bdf7798598",
+    "url": "/MyPage/index.html"
+  },
+  {
+    "revision": "4a56ee556f15094acc03",
+    "url": "/MyPage/static/js/2.d47fbb6b.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/MyPage/static/js/2.d47fbb6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911d81a3405e40e74c58",
+    "url": "/MyPage/static/js/main.7a6a3b04.chunk.js"
+  },
+  {
+    "revision": "4f832e9cb003ce343b34",
+    "url": "/MyPage/static/js/runtime-main.7e37247a.js"
+  },
+  {
+    "revision": "803bab917eaa49febd5165c4c2a2c217",
+    "url": "/MyPage/static/media/profile_img.803bab91.JPG"
+  }
+]);
