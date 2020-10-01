7bc48b2fecb9f168fe98efadb9797de3bc280668
BB <seven.albany.bi@gmail.com>
2020-10-01 23:27:49 +0800

    Updates

---
+++ node_modules/.cache/gh-pages/github.com!Seven-Bi!MyPage.git/precache-manifest.744f8895dc1523553d3df9c388ec26a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eec0693e7001540fc2b2a2815dce47f",
+    "url": "/MyPage/index.html"
+  },
+  {
+    "revision": "4a56ee556f15094acc03",
+    "url": "/MyPage/static/js/2.d47fbb6b.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/MyPage/static/js/2.d47fbb6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e90b8e40ed66101f45",
+    "url": "/MyPage/static/js/main.ceec5f19.chunk.js"
+  },
+  {
+    "revision": "4f832e9cb003ce343b34",
+    "url": "/MyPage/static/js/runtime-main.7e37247a.js"
+  },
+  {
+    "revision": "352930e569d2a5b23cf8e11570131caa",
+    "url": "/MyPage/static/media/new_profile_img.352930e5.jpg"
+  }
+]);
